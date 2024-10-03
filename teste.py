7cac031483f9979d314ef21b096d375e722d6c8a
Arthur <arthursaraiva51@gmail.com>
2024-10-03 16:25:52 -0300

    att uma linha no codigo

--- teste.py
+++ teste.py
@@ -4,3 +4,11 @@ if idade >= 18:
     print("Você pode dirigir.")
 else:
     print("Você não pode dirigir.")
+
+    feijoada = True
+
+    if feijoada:
+        print("Usar feijoada.")
+    else:
+        print("Usar arroz.")
+    
